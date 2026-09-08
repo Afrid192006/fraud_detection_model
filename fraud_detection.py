7ef0ebe5cbac46fa9e70aefe32056b957a7e0055
Afrid192006 <singapuriafrid@gmail.com>
2026-09-08 12:04:56 +0530

    Path changes

--- fraud_detection.py
+++ fraud_detection.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("fraud_detection_pipeline.pkl")
+model = joblib.load("model\fraud_detection_pipeline.pkl")
 
 st.title("Fraud Detection Prediction App")
 
